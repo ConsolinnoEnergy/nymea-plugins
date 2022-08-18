c9eee1ce1b2950f08e2b0c37c7b09289d57ba862
l.heizinger <l.heizinger@consolinno.de>
2022-08-18 13:44:53 +0000

    Update integrationpluginsgready.cpp

--- sgready/integrationpluginsgready.cpp
+++ sgready/integrationpluginsgready.cpp
@@ -93,7 +93,7 @@ void IntegrationPluginSgReady::setupThing(ThingSetupInfo *info)
             Q_UNUSED(mode)
             thing->setStateValue(sgReadyInterfaceGpio1StateStateTypeId, sgReadyInterface->gpio1()->value() == Gpio::ValueHigh);
             thing->setStateValue(sgReadyInterfaceGpio2StateStateTypeId, sgReadyInterface->gpio2()->value() == Gpio::ValueHigh);
-            thing->setStateValue(sgReadyInterfacesgReadyModeStateTypeId, sgReadyInterface->sgReadyMode());
+            thing->setStateValue(sgReadyInterfaceSgReadyModeStateTypeId, sgReadyInterface->sgReadyMode());
         });
 
         m_sgReadyInterfaces.insert(thing, sgReadyInterface);
