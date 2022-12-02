11345897cd807a03fd9869f255061a644e386b4c
Felix <f.stoecker@consolinno.de>
2022-12-02 16:59:44 +0100

    implemented base classes for connection build and request handle
    Base classes providing functionality to connect to fems and also get the NetworkReply with data provided by NetworkAccessManager
    Supports FEMS and OpenEmsEdge
    
    Main Functionality: getFemsReply(QString Path) 
    This will get the Networkreply depending on the given Path (componentID/channelId)

--- fems/femsconnection.h
+++ fems/femsconnection.h
@@ -3,14 +3,69 @@
 
 #include <QObject>
 
+#include <QQueue>
+#include <QHostAddress>
+
+#include <network/networkaccessmanager.h>
+
+#include "femsnetworkreply.h"
+
+
 class FemsConnection : public QObject
 {
     Q_OBJECT
 public:
-    explicit FemsConnection(QObject *parent = nullptr);
+    explicit FemsConnection(NetworkAccessManager *networkManager,
+                            const QHostAddress &address, QObject *parent = nullptr,
+                            QString user, QString pwd, bool useEdge, QString port);
+
+    QHostAddress address() const;
+
+    bool available() const;
+
+    bool busy() const;
+
+    FemsNetworkReply *isAvailable();
+    //Has to be called by integrationsPlugin for each datapoint that you want to receive
+    FemsNetworkReply *getFemsDataPoint(QString componentAndChannelId);
+
+    enum StateOfFems{
+        OK = 0,
+        INFO = 1,
+        WARNING = 2,
+        FAULT = 3
+    };
+
 
 signals:
+
+    void availableChanged(bool available);
+
+private:
+    NetworkAccessManager *m_networkManager = nullptr;
+    QHostAddress m_address;
+
+    QString m_user = "";
+    QString m_password = "";
+    QString m_port = "80";
+    QString baseUrl;
+
+    bool m_use_edge = false;
+    bool batteryAvailable = false;
+    bool meterAvailable = false;
+    bool m_available = false;
+
+    FemsNetworkReply *m_currentReply = nullptr;
+    QQueue<FemsNetworkReply *> m_request_Queue;
+    FemsNetworkReply *getFemsReply(QString path);
+
+    void sendNextRequest();
+
+
+
 
 };
 
 #endif // FEMSCONNECTION_H
+
+
