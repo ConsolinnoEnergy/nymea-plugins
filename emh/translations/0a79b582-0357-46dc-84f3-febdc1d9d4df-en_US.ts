689f97f1e1935bbcc479d9af985a001699b4fe28
Leonid Verhovskij <l.verhovskij@consolinno.de>
2024-06-12 16:07:14 +0200

    add translations for emh

---
+++ emh/translations/0a79b582-0357-46dc-84f3-febdc1d9d4df-en_US.ts
@@ -0,0 +1,97 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>emh</name>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="48"/>
+        <source>IP address of SMGW</source>
+        <extracomment>The name of the discovery parameter (id: fa98fc68-52d0-4d92-ae7a-54fb7b7ec813) of ThingClass emhHanInterface</extracomment>
+        <translation>IP address of SMGW</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="51"/>
+        <source>HKS2 Username</source>
+        <extracomment>The name of the discovery parameter (id: fd42379e-304b-4a76-820d-2e2ee553bb4b) of ThingClass emhHanInterface</extracomment>
+        <translation>HKS2 Username</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="54"/>
+        <source>HKS2 Password</source>
+        <extracomment>The name of the discovery parameter (id: 389fb63e-6c69-4187-929e-5856b81561a7) of ThingClass emhHanInterface</extracomment>
+        <translation>HKS2 Password</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="57"/>
+        <source>HUID of smart meter</source>
+        <extracomment>The name of the parameter (id: d765dc0f-9a1a-4cfb-9a81-d47029c63355) of ThingClass emhHanInterface</extracomment>
+        <translation>HUID of smart meter</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="60"/>
+        <source>HUID of SMGW</source>
+        <extracomment>The name of the parameter (id: 2b5c16a3-ff85-4e33-b017-94e3db496758) of ThingClass emhHanInterface</extracomment>
+        <translation>HUID of SMGW</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="63"/>
+        <source>ID of HKS2 user at SMGW</source>
+        <extracomment>The name of the parameter (id: 1a2c3560-37fe-48aa-b154-2700d33640cc) of ThingClass emhHanInterface</extracomment>
+        <translation>ID of HKS2 user at SMGW</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="66"/>
+        <source>IP address of SMGW</source>
+        <extracomment>The name of the parameter (id: 92e75c6a-5a12-4b36-af53-9c19a4cbab00) of ThingClass emhHanInterface</extracomment>
+        <translation>IP address of SMGW</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="69"/>
+        <source>HKS2 Username</source>
+        <extracomment>The name of the parameter (id: 23d533e5-4e1b-4bcf-ae7b-fd22e88466a9) of ThingClass emhHanInterface</extracomment>
+        <translation>HKS2 Username</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="72"/>
+        <source>HKS2 Password</source>
+        <extracomment>The name of the parameter (id: 00f39173-4922-4d3e-be2e-cde7130ae40d) of ThingClass emhHanInterface</extracomment>
+        <translation>HKS2 Password</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="75"/>
+        <source>Connected</source>
+        <extracomment>The name of the state (id: 07b275f6-2505-40d0-93a9-8d4e6a254198) of ThingClass emhHanInterface</extracomment>
+        <translation>Connected</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="78"/>
+        <source>Current power (OBIS 16.7.0)</source>
+        <extracomment>The name of the state (id: 58f90639-e4a0-44e2-ba98-b1cfe272f7fb) of ThingClass emhHanInterface</extracomment>
+        <translation>Current power (OBIS 16.7.0)</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="81"/>
+        <source>Total energy produced (OBIS 2.8.0)</source>
+        <extracomment>The name of the state (id: 454bd0d8-3576-49a3-911d-8c769ad9d53e) of ThingClass emhHanInterface</extracomment>
+        <translation>Total energy produced (OBIS 2.8.0)</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="84"/>
+        <source>Total energy consumed (OBIS 1.8.0)</source>
+        <extracomment>The name of the state (id: d01feda3-410b-4abe-82f8-de6a6ce03500) of ThingClass emhHanInterface</extracomment>
+        <translation>Total energy consumed (OBIS 1.8.0)</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="87"/>
+        <source>Current phase A</source>
+        <extracomment>The name of the state (id: 718d4229-ec53-4de5-936c-af6af3af0570) of ThingClass emhHanInterface</extracomment>
+        <translation>Current phase A</translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/emh/plugininfo.h" line="90"/>
+        <source>Voltage phase A</source>
+        <extracomment>The name of the state (id: 625fe00d-8614-42e9-8bf0-472f339c69cf) of ThingClass emhHanInterface</extracomment>
+        <translation>Voltage phase A</translation>
+    </message>
+</context>
+</TS>
