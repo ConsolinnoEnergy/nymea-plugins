c2ee89bf179066f3f496121f08522ec9d5a15a48
Luis Reber <l.reber@consolinno.de>
2024-08-02 09:05:55 +0200

    Copied plugin from nymea

---
+++ easee/translations/471aa296-78de-4917-84ed-c9a4216f5ae9-de.ts
@@ -0,0 +1,157 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>IntegrationPluginEasee</name>
+    <message>
+        <location filename="../integrationplugineasee.cpp" line="70"/>
+        <source>Authentication failed. Please try again.</source>
+        <translation>Authentifizierung fehlgeschlagen. Bitte versuchen Sie es erneut.</translation>
+    </message>
+    <message>
+        <location filename="../integrationplugineasee.cpp" line="75"/>
+        <source>Unable to contact the easee server. Please try again later.</source>
+        <translation>Verbindung zum Easee Server fehlgeschlagen. Bitte versuchen Sie es später erneut.</translation>
+    </message>
+    <message>
+        <location filename="../integrationplugineasee.cpp" line="84"/>
+        <source>Unable to process the response from easee. Please try again later.</source>
+        <translation>Die Antwort von Easee konnte nicht verarbeitet werden. Bitte versuchen Sie es später erneut.</translation>
+    </message>
+</context>
+<context>
+    <name>easee</name>
+    <message>
+        <location filename="../plugininfo.h" line="49"/>
+        <source>Charger ID</source>
+        <extracomment>The name of the ParamType (ThingClass: charger, Type: thing, ID: {b9f0573e-bf41-45f0-a53e-b2457d51ecb5})</extracomment>
+        <translation>Wallbox ID</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="52"/>
+        <source>Charging</source>
+        <extracomment>The name of the StateType ({d95137ad-4f32-4ffd-851b-593fc40f5c0c}) of ThingClass charger</extracomment>
+        <translation>Lädt</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="55"/>
+        <source>Connected</source>
+        <extracomment>The name of the StateType ({568d5896-813e-4f66-b430-ed8c8b1ad8c4}) of ThingClass account</extracomment>
+        <translation>Verbunden</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="58"/>
+        <source>Current charting power</source>
+        <extracomment>The name of the StateType ({6b0c22e9-cdb4-49a0-9c18-97b776b63c59}) of ThingClass charger</extracomment>
+        <translation>Aktuelle Ladeleistung</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="61"/>
+        <location filename="../plugininfo.h" line="64"/>
+        <source>Desired phase count</source>
+        <extracomment>The name of the ParamType (ThingClass: charger, ActionType: desiredPhaseCount, ID: {1baac39e-0e0e-4638-bd8d-09f04fd7bd62})
+----------
+The name of the StateType ({1baac39e-0e0e-4638-bd8d-09f04fd7bd62}) of ThingClass charger</extracomment>
+        <translation>Gewünschte Phasenzahl</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="67"/>
+        <location filename="../plugininfo.h" line="70"/>
+        <source>Easee</source>
+        <extracomment>The name of the vendor ({e43f66da-4e8c-4e2c-b821-c1eb8ac6bbf8})
+----------
+The name of the plugin easee ({471aa296-78de-4917-84ed-c9a4216f5ae9})</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="73"/>
+        <source>Easee account</source>
+        <extracomment>The name of the ThingClass ({2c93c25e-d12a-4709-b537-b5619ab1145a})</extracomment>
+        <translation>Easee Konto</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="76"/>
+        <source>Easee charger</source>
+        <extracomment>The name of the ThingClass ({9fe4d708-70d2-47bd-8433-54c5f3e8a110})</extracomment>
+        <translation>Easee Wallbox</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="79"/>
+        <source>Logged in</source>
+        <extracomment>The name of the StateType ({69d01e58-c1a3-4b9c-a4b3-f1de7c67febd}) of ThingClass account</extracomment>
+        <translation>Eingeloggt</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="82"/>
+        <location filename="../plugininfo.h" line="85"/>
+        <source>Maximum charging current</source>
+        <extracomment>The name of the ParamType (ThingClass: charger, ActionType: maxChargingCurrent, ID: {1c818574-5aa8-43f5-8fa5-9a620ba86ccc})
+----------
+The name of the StateType ({1c818574-5aa8-43f5-8fa5-9a620ba86ccc}) of ThingClass charger</extracomment>
+        <translation>Maximaler Ladestrom</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="88"/>
+        <source>Online</source>
+        <extracomment>The name of the StateType ({2a9b4c84-7b1d-4b32-b26f-84eff54fb04f}) of ThingClass charger</extracomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="91"/>
+        <source>Plugged in</source>
+        <extracomment>The name of the StateType ({0ac13ef2-e646-491f-9336-f39d5110f8bb}) of ThingClass charger</extracomment>
+        <translation>Eingesteckt</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="94"/>
+        <location filename="../plugininfo.h" line="97"/>
+        <source>Power</source>
+        <extracomment>The name of the ParamType (ThingClass: charger, ActionType: power, ID: {b5070800-3556-4156-aed5-ecbad7fa455d})
+----------
+The name of the StateType ({b5070800-3556-4156-aed5-ecbad7fa455d}) of ThingClass charger</extracomment>
+        <translation>Laden aktiviert</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="100"/>
+        <source>Session energy</source>
+        <extracomment>The name of the StateType ({2f0c3491-d524-4ee5-8214-8e3d2f750b66}) of ThingClass charger</extracomment>
+        <translation>Aktuell geladene Energie</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="103"/>
+        <source>Set desired phase count</source>
+        <extracomment>The name of the ActionType ({1baac39e-0e0e-4638-bd8d-09f04fd7bd62}) of ThingClass charger</extracomment>
+        <translation>Setze gewünschte Phasenanzahl</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="106"/>
+        <source>Set maximum charging current</source>
+        <extracomment>The name of the ActionType ({1c818574-5aa8-43f5-8fa5-9a620ba86ccc}) of ThingClass charger</extracomment>
+        <translation>Setze maximalen Ladestrom</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="109"/>
+        <source>Set power</source>
+        <extracomment>The name of the ActionType ({b5070800-3556-4156-aed5-ecbad7fa455d}) of ThingClass charger</extracomment>
+        <translation>Laden aktiviert</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="112"/>
+        <source>Signal strength</source>
+        <extracomment>The name of the StateType ({7effe12e-2884-4597-ad0c-6b4aa9cdacfd}) of ThingClass charger</extracomment>
+        <translation>Signalstärke</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="115"/>
+        <source>Total energy consumed</source>
+        <extracomment>The name of the StateType ({b55297a8-f1bd-44eb-8309-691d674ef4a0}) of ThingClass charger</extracomment>
+        <translation>Gesamt geladene Energie</translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="118"/>
+        <source>Used phases</source>
+        <extracomment>The name of the StateType ({3a68d6c5-d48f-4cd6-b9ca-fc21cbcc8808}) of ThingClass charger</extracomment>
+        <translation>Phasen</translation>
+    </message>
+</context>
+</TS>
